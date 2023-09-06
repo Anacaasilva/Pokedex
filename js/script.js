9fccbea706207b9d5401ec29bddd4466b6e3b921
Ana Carolina <anacarolina.oliveira1020@gmail.com>
2023-09-06 15:47:39 -0300

    Finalizando Pokédex

--- js/script.js
+++ js/script.js
@@ -1,23 +1,63 @@
 const pokemonImage = document.getElementById('pokemonImage');
 const pokemonNumber = document.getElementById('pokemonNumber');
-const pokemonName = document.getElementById('pokemonName') 
+const pokemonName = document.getElementById('pokemonName');
+
+const form = document.querySelector('.form');
+const input = document.querySelector('.inputSearch');
+const buttonPrev = document.querySelector('.btn-prev');
+const buttonNext = document.querySelector('.btn-next');
+
+let searchPokemon = 1;
+
 
 const fetchPokemon = async (pokemon) => {
   const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
-  const data = await APIResponse.json();
 
-  return data;
+  if (APIResponse.status === 200) {
+    const data = await APIResponse.json();
+    return data;
+  }
 }
 
 const renderPokemon = async (pokemon) => {
+  pokemonName.innerHTML = `Loading ...`;
+  pokemonNumber.innerHTML = '';
+
   const data = await fetchPokemon(pokemon);
 
-  pokemonName.innerHTML = data.name;
-  pokemonNumber.innerHTML = data.id;
-  pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+  if (data) {
+    pokemonImage.style.display = 'block';
+    pokemonName.innerHTML = data.name;
+    pokemonNumber.innerHTML = data.id;
+    pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
 
+    input.value = '';
+    searchPokemon = data.id;
+  } else {
+    pokemonName.innerHTML = `Not found :c`
+    pokemonNumber.innerHTML = '';
+    pokemonImage.style.display = 'none';
+
+    input.value = '';
+  }
 }
 
-renderPokemon('25')
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
 
-"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/25.gif"
+  renderPokemon(input.value.toLowerCase()); //fazer com que todas as letras fiquem minusculas
+});
+
+buttonPrev.addEventListener('click', () => {
+  if (searchPokemon > 1) {
+    searchPokemon -= 1;
+    renderPokemon(searchPokemon);
+  }
+});
+
+buttonNext.addEventListener('click', () => {
+  searchPokemon += 1;
+  renderPokemon(searchPokemon);
+})
+
+renderPokemon(searchPokemon);
